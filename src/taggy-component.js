a761699b9c955cc9363d0213b808e68f7af98f51
diegograsso <diego.grasso@gmail.com>
2016-04-21 23:31:42 -0300

    Added more options

--- src/taggy-component.js
+++ src/taggy-component.js
@@ -6,8 +6,21 @@ export class TaggyComponent {
   @bindable ceId;
   @bindable eventChannel;
   @bindable minChars;
-  @bindable api;
   @bindable placeholder;
+  @bindable free;
+  @bindable deletion;
+  @bindable delimiter;
+  @bindable preventInvalid;
+  @bindable validate;
+  @bindable render;
+  @bindable convertOnBlur;
+  @bindable parseText;
+  @bindable autocomplete;
+
+  @processContent(function(viewCompiler, viewResources, element, instruction) {
+    instruction.autocomplete = processAutoComplete(element);
+    return true;
+  })
 
   static inject(){return [EventAggregator]}
   constructor(ea) {
@@ -25,13 +38,19 @@ export class TaggyComponent {
 
   _initTaggy(){
     this.taggyElement =  taggy(document.querySelector("#"+this.ceId), {
-      deletion: true,
-      parseText: 'name',
-      autocomplete: {
-        noMatches: 'No results found.',
-        suggestions: this.api,
-      },
-      free: false
+       deletion: this.deletion,
+       ceId: this.ceId,
+       eventChannel: this.eventChannel,
+       minChars:minChars,
+       placeholder:placeholder,
+       free: free,
+       delimiter: delimiter,
+       preventInvalid: preventInvalid,
+       validate: validate,
+       render:render,
+       convertOnBlur:convertOnBlur,
+       parseText:parseText,
+       autocomplete:autocomplete
     });
     this.taggyElement.on("add", (item) => {this.onSelectedTokensChange()});
     this.taggyElement.on("remove", (item) => {this.onSelectedTokensChange()});
