86d16f38b6cb0dfdcaaa3a4919ae9f2bf2bf80f7
diegograsso <diego.grasso@gmail.com>
2016-05-26 17:35:06 -0300

    Updated taggy version.

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ System.config({
 
   map: {
     "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0-beta.1.1.6",
-    "collokia/taggy": "github:collokia/taggy@1.0.1",
+    "collokia/taggy": "github:collokia/taggy@1.0.2",
     "npm:aurelia-polyfills@1.0.0-beta.1.1.6": {
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.2"
     }
